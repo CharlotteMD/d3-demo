3ab682bbb5b6edfd43f2230a5734a03d977e9a7e
Charlotte Qazi <charlottemdavies@hotmail.co.uk>
2022-04-08 16:41:40 +0100

    page/level one

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import StepOne from './containers/stepOne';
+import StepTwo from './containers/stepTwo';
 import Home from './containers/home';
 
 function App() {
   return (
     <div className="App">
-      <h1>D3 demo</h1>
+      <h1 style={{textAlign: 'left'}}>D3 demo</h1>
       <Router>
         <Routes>
             <Route path="/step-one" element={<StepOne/>}/>
+            <Route path="/step-two" element={<StepTwo/>}/>
             <Route path="/" element={<Home/>}/>
           </Routes>
       </Router>
--- src/containers/stepOne.js
+++ src/containers/stepOne.js
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as d3 from "d3";
+import * as fcAxis from '@d3fc/d3fc-axis';
+import * as fcAnnotation from '@d3fc/d3fc-annotation';
+import data from '../data.json';
+
+// set the dimensions and margins of the graph
+const maxDesktopWidth = 1400;
+const viewHeight = 800;
+const margin = {top: 20, right: 30, bottom: 50, left: 50};
+const width = maxDesktopWidth - margin.left - margin.right;
+const height = viewHeight - margin.top - margin.bottom;
 
 const StepOne = () => {
+  const [ universe, setUniverse ] = useState([]);
+
+  const clearOldGraphs = () => {
+    const svg = d3.select('#graph')
+          svg.selectAll("*").remove()
+  }
+
+  const dGraph = () => {
+
+    // append the svg object to the body of the page
+    const svg = 
+      d3.select("#graph")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+
+    // append the space for the graph (minus the axis) (called circle-container)
+    svg.append('g')
+      .attr("id", "circle-container")
+      .selectAll("dot")
+      .data(universe)
+      .enter();
+    
+    // addXAxis(svg);
+    // addYAxis(svg);
+    gridLines();
+  }
+    
+  const gridLines = () => {
+
+    // set up blank graph
+    const xScale = 
+      d3.scaleLinear()
+        .domain([0, 4])
+        .range([ 0, width ]);
+
+    const yScale = 
+      d3.scaleLinear()
+        .domain([0, 3])
+        .range([ height, 0]);
+
+    // add gridlines using d3 extension fc
+    fcAxis.axisLabelRotate(fcAxis.axisOrdinalLeft(yScale));
+
+    // add ticks in the same scale as axis
+    var gridline = fcAnnotation
+      .annotationSvgGridline()
+        .xScale(xScale)
+        .yScale(yScale);
+    
+    // add correct number of ticks
+    gridline.xTicks([4]);
+    gridline.yTicks([3]);
+
+    // append the gridlines to the circle-container 
+    d3.select('#circle-container')
+        .call(gridline);
+  }
+
+  useEffect(() => {
+    setUniverse(data);
+  },[]);
+
+  useEffect(() => {
+    clearOldGraphs();
+    console.log(universe);
+    dGraph();
+  },[universe])
+
   return (
-    <div className="App">
-      <h1>Step One</h1>
-    </div>
+    <>
+        <h2>Step one.</h2>
+        <h1>Draw a blank graph</h1>
+        <div id="graph"></div> 
+    </>
   );
 }
 
